00c59b229d0beced9000c9b7d0b6a616f9321a48
hobbyquaker <hq@ccu.io>
2017-05-02 18:55:48 +0200

    don't try to parse non-existant file, fix lint issues

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-var ppath =     require('persist-path');
-var fs =        require('fs');
-var mkdirp =    require('mkdirp');
+var fs = require('fs');
+var ppath = require('persist-path');
+var mkdirp = require('mkdirp');
 
 function Pjson(folder) {
-    if (!(this instanceof Pjson)) return new Pjson(folder);
-    if (typeof folder !== 'string' || folder === '') throw new Error('param folder missing');
+    if (!(this instanceof Pjson)) {
+        return new Pjson(folder);
+    }
+    if (typeof folder !== 'string' || folder === '') {
+        throw new Error('param folder missing');
+    }
     this.folder = folder;
     var p = ppath(folder);
     mkdirp(p);
 }
 
 Pjson.prototype.save = function (file, content, cb) {
-    if (typeof file !== 'string' || file === '') throw new Error('param file missing');
+    if (typeof file !== 'string' || file === '') {
+        throw new Error('param file missing');
+    }
     var json = JSON.stringify(content, null, '  ');
     file = ppath(this.folder, file);
     if (typeof cb === 'function') {
@@ -22,22 +28,27 @@ Pjson.prototype.save = function (file, content, cb) {
 };
 
 Pjson.prototype.load = function (file, cb) {
-    if (typeof file !== 'string' || file === '') throw new Error('param file missing');
+    if (typeof file !== 'string' || file === '') {
+        throw new Error('param file missing');
+    }
     var f = ppath(this.folder, file);
     if (typeof cb === 'function') {
         fs.readFile(f, function (err, res) {
-            if (err) cb(err, res);
-            try {
-                var obj = JSON.parse(res);
-                cb(null, obj);
-            } catch (e) {
-                cb(e)
+            if (err) {
+                cb(err);
+            } else {
+                try {
+                    var obj = JSON.parse(res);
+                    cb(null, obj);
+                } catch (err) {
+                    cb(err);
+                }
             }
         });
     } else {
         try {
             return JSON.parse(fs.readFileSync(f));
-        } catch (e) {
+        } catch (err) {
             return undefined;
         }
     }
