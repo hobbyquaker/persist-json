1e10d8e34b9358d87f7404427cc0a34d02779c46
hobbyquaker <hq@ccu.io>
2017-05-02 18:58:46 +0200

    add linting, testing and coverage

--- test.js
+++ test.js
@@ -1,14 +1,48 @@
-var pjson = require('./index.js')('ppath-test');
+var ppath = require('persist-path');
+var rand = Math.random().toString(36).slice(2);
+var name = 'persist-json-test-' + rand;
+var path = ppath(name);
 
-// sync
-pjson.save('settings.json', {'test': 'sync'});
-console.log(pjson.load('settings.json'));
+var should = require('should');
 
-// async
-pjson.save('settings.json', {'test': 'async'}, function (err, res) {
-    if (err) throw err;
-    pjson.load('settings.json', function (err, res) {
-        if (err) throw err;
-        console.log(res);
+console.log('test path:', path);
+
+var pjson;
+
+describe('instantiate', function () {
+    it('should instantiate without error', function () {
+        pjson = require('./index.js')(name);
+    });
+});
+
+describe('synchronously save and load', function () {
+    it('should synchronously save data', function () {
+        should(pjson.save('sync', {a: true})).equal(undefined);
+    });
+    it('should synchronously load data', function () {
+        should(pjson.load('sync', {a: true})).deepEqual({a: true});
+    });
+});
+
+describe('asynchronously save and load', function () {
+    it('should asynchronously save data', function (done) {
+        pjson.save('async', {a: true}, done);
+    });
+    it('should asynchronously load data', function () {
+        pjson.load('async', {a: true}, function (err, res) {
+            res.should.deepEqual({a: true});
+            done(err);
+        });
+    });
+});
+
+describe('error handling', function () {
+    it('should return undefined if synchronously trying to load non existing file', function () {
+        should(pjson.load('missing')).equal(undefined);
+    });
+    it('should callback with error if asynchronously trying to load non existing file', function (done) {
+        pjson.load('missing', function (err, data) {
+            done(!err);
+        });
     });
 });
